529f6090466771243f1a709d50beb5384543b66f
Peter Imini <peterimini2@gmail.com>
2026-07-04 13:12:09 +0100

    create rock, paper, scissors fuul game

--- js/demo-2.js
+++ js/demo-2.js
@@ -5,9 +5,9 @@ console.log(Math.pow(2, 3));
 
 console.log("Peter".charAt(3)); 
 
-// Tenary operators (If..else)
 
-let testScore = 73; //Test grading system
+//Test grading system
+let testScore = 73; 
 let myGrade =
   testScore >= 90 ? "A" :
   testScore >= 80 ? "B" :
@@ -17,7 +17,8 @@ let myGrade =
   "F";
 console.log(`My test grade is ${myGrade}.`);
 
-let playerOne = "paper";
+// Rock, paper, scissors game
+let playerOne = "paper"; 
 let computer = "rock"
 let result =
   playerOne === computer ? "Tie!"
@@ -27,7 +28,57 @@ let result =
         : "playerOne wins!";
 console.log(result);
 
+confirm("Ok === True\nCancel === False");
 
- 
+ //User Input
+let name = prompt("Please enter your name.");
+if (name) {
+  console.log(name.length);
+  console.log(name.trim().length);
+  console.log(name.trim());
+  console.log(name);
+} else {
+  console.log("You didn't enetr your name.");
+}
 
- 
+My First Interactive game 
+let playGame = confirm("Shall we play rock, paper, scissors?");
+if (playGame) {
+  //play
+  let playerChoice = prompt("Please enter rock, paper, or scissors.");
+  if (playerChoice) {
+    let playerOne = playerChoice.trim().toLowerCase();
+    if (playerOne === "rock" || playerOne === "paper" ||
+      playerOne === "scissors") {
+
+  let computerChoice = Math.floor(Math.random() * 3 + 1);
+  let computer = computerChoice === 1 ? "rock"
+    : computerChoice === 2 ? "paper"
+    : "scissors"
+      
+      let result =
+        playerOne === computer
+          ? "Tie!"
+          : playerOne === "rock" && computer === "paper"
+            ? `playerOne: ${playerOne}\nComputer: ${computer}
+      \nComputer wins!`
+            : playerOne === "paper" && computer === "scissors"
+              ? `playerOne: ${playerOne}\nComputer: ${computer}
+      \nComputer wins!`
+              : playerOne === "scissors" && computer === "rock"
+                ? `playerOne: ${playerOne}\nComputer: ${computer}
+      \nComputer wins!`
+                : `playerOne: ${playerOne}\nComputer: ${computer}
+      \nPlayerOne wins!`;
+      alert(result);
+      let playAgain = confirm("Play Again?");
+      playAgain ? location.reload() : alert("Ok, thanks for playing");
+    } else {
+      alert("You didn't choose any.");
+    }
+  } else {
+    alert("Guesss you changed your mind, maybe next time.");
+  }
+} else {
+  alert("Ok, maybe next time.");
+}
